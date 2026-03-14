1e5d7445ac6f3d062eb5ef60e1c5e199e37d456b
Raghuldurai <raghuldurai3000@gmail.com>
2026-03-14 18:47:15 +0530

    working perfectly

--- home-service/backend/src/server.js
+++ home-service/backend/src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const productRoutes = require("./routes/productRoutes");
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", productRoutes);
+
+// Serve static files for images
+app.use('/images', express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Mongo connected"))
